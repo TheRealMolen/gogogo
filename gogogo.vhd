7e78ffab4d8046659aedcb9fdbecd8db1856eeb7
molen <alex.molen@gmail.com>
2014-08-10 18:12:03 +0100

    bottom 4 switches val shown on 7seg display

--- gogogo.vhd
+++ gogogo.vhd
@@ -2,36 +2,67 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
-
 entity gogogo is
-    Port ( switches : in STD_LOGIC_VECTOR( 0 downto 0 );
-           LEDs : out  STD_LOGIC_VECTOR( 7 downto 0 );
+    Port ( switches : in STD_LOGIC_VECTOR( 3 downto 0 );
+           digits : out  STD_LOGIC_VECTOR( 3 downto 0 );
+			  segments : out  STD_LOGIC_VECTOR( 6 downto 0 );
+			  dp : out STD_LOGIC;
 			  clk : in STD_LOGIC);
 end gogogo;
 
 architecture Behavioral of gogogo is
 	signal counter : UNSIGNED( 29 downto 0 ) := (others => '0');
-	signal seconds : UNSIGNED( 7 downto 0 ) := (others => '0');
 begin
 
-	tick: process( clk, switches( 0 ) )
+	tick: process( clk )
 	begin
-		if switches(0) = '0' then
-			counter <= (others => '0');
-		else
-			if rising_edge(clk) then
-				-- if we've hit a second, reset counter and inc seconds
-				if counter = to_unsigned(32000000, counter'length) then
-					seconds <= seconds + 1;
-					counter <= (others => '0');
-				else
-					counter <= counter + 1;
-				end if;
-			end if;
+		if rising_edge(clk) then
+			counter <= counter + 1;
 		end if;
 	end process;
 	
-	LEDs <= std_logic_vector( seconds );
+	numberise: process( switches )
+	begin
+		case switches( 3 downto 0 ) is
+			when "0000" =>
+				segments <= "1000000";
+			when "0001" =>
+				segments <= "1111001";
+			when "0010" =>
+				segments <= "0100100";
+			when "0011" =>
+				segments <= "0110000";
+			when "0100" =>
+				segments <= "0011001";
+			when "0101" =>
+				segments <= "0010010";
+			when "0110" =>
+				segments <= "0000010";
+			when "0111" =>
+				segments <= "1111000";
+			when "1000" =>
+				segments <= "0000000";
+			when "1001" =>
+				segments <= "0010000";
+			when "1010" =>
+				segments <= "0001000";
+			when "1011" =>
+				segments <= "0000011";
+			when "1100" =>
+				segments <= "0100111";
+			when "1101" =>
+				segments <= "0100001";
+			when "1110" =>
+				segments <= "0000110";
+			when "1111" =>
+				segments <= "0001110";
+			
+			when others =>
+				segments <= (others => '0');
+		end case;
+	end process;
+	
+	digits <= "0000";	-- ALL THE DIGITS!!
+	dp <= '1';
 	
 end Behavioral;
