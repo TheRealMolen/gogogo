a317b0231794066eadf45056363c36519dda25e4
molen <alex.molen@gmail.com>
2014-08-10 16:19:28 +0100

    count up or down depending on switches

--- gogogo.vhd
+++ gogogo.vhd
@@ -5,7 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity gogogo is
-    Port ( switches : in STD_LOGIC_VECTOR( 7 downto 0 );
+    Port ( switches : in STD_LOGIC_VECTOR( 1 downto 0 );
            LEDs : out  STD_LOGIC_VECTOR( 7 downto 0 );
 			  clk : in STD_LOGIC);
 end gogogo;
@@ -14,33 +14,21 @@ architecture Behavioral of gogogo is
 	signal counter : UNSIGNED( 29 downto 0 ) := (others => '0');
 begin
 
-	tick: process( clk )
+	tick: process( clk, switches( 1 downto 0 ) )
 	begin
-		if rising_edge(clk) then
-			counter <= counter + 1;
+		if switches(0) = '0' then
+			counter <= (others => '0');
+		else
+			if rising_edge(clk) then
+				if switches(1) = '0' then
+					counter <= counter - 1;
+				else
+					counter <= counter + 1;
+				end if;
+			end if;
 		end if;
 	end process;
 	
 	LEDs <= std_logic_vector( counter(29 downto 22) );
 	
 end Behavioral;
-
---
---architecture Behavioral of gogogo is
---  signal x      : UNSIGNED(3 downto 0);
---  signal y      : UNSIGNED(3 downto 0);
---  signal result : UNSIGNED(4 downto 0);
---begin
---
---	-- show 5 bit result on LEDs
---	LEDs <= "000" & std_logic_vector(result);
---
---	-- read 2 4 bit numbers from switches
---	x <= unsigned( switches(3 downto 0) );
---	y <= unsigned( switches(7 downto 4) );
---	
---	result(4 downto 0) <= ('0' & x) + y;
---	
---end Behavioral;
---
-
