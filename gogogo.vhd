349152070455ee8f24ca393e157561f5f481f013
molen <alex.molen@gmail.com>
2014-08-16 20:10:42 +0100

    decimal counter
    LESSON: signals are only updated at the end of a process, so we loop
    digits when they hit 9 and they’ve been incremented

--- gogogo.vhd
+++ gogogo.vhd
@@ -2,6 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+
+
 entity gogogo is
     Port ( --switches : in STD_LOGIC_VECTOR( 7 downto 0 );
            digits : out  STD_LOGIC_VECTOR( 3 downto 0 );
@@ -23,24 +25,78 @@ architecture Behavioral of gogogo is
 		segments : OUT std_logic_vector(6 downto 0)
 		);
 	END COMPONENT;
+	
+	
+	function  divide  (a : UNSIGNED; b : UNSIGNED) return UNSIGNED is
+		variable a1 : unsigned(a'length-1 downto 0):=a;
+		variable b1 : unsigned(b'length-1 downto 0):=b;
+		variable p1 : unsigned(b'length downto 0):= (others => '0');
+		variable i : integer:=0;
 
-	signal counter : UNSIGNED( 33 downto 0 ) := (others => '0');
+	begin
+		for i in 0 to b'length-1 loop
+			p1(b'length-1 downto 1) := p1(b'length-2 downto 0);
+			p1(0) := a1(a'length-1);
+			a1(a'length-1 downto 1) := a1(a'length-2 downto 0);
+			p1 := p1-b1;
+			if(p1(b'length-1) ='1') then
+				a1(0) :='0';
+				p1 := p1+b1;
+			else
+				a1(0) :='1';
+			end if;
+		end loop;
+		
+		return a1;
+	end divide;
+
+	signal counter : UNSIGNED( 21 downto 0 ) := (others => '0');
+	signal digit0 : UNSIGNED( 3 downto 0 ) := (others => '0');
+	signal digit1 : UNSIGNED( 3 downto 0 ) := (others => '0');
+	signal digit2 : UNSIGNED( 3 downto 0 ) := (others => '0');
+	signal digit3 : UNSIGNED( 3 downto 0 ) := (others => '0');
 begin
 
 	tick: process( clk )
 	begin
 		if rising_edge(clk) then			
 			counter <= counter + 1;
+			
+			-- has counter hit max?
+			if counter(21) = '1' then
+				digit0 <= digit0 + 1;
+				counter <= (others => '0');
+				
+				-- trickle all the way down....
+				-- WHY 9 AND NOT 10? the comparison must happen before the increment, but aren't processes sequential?
+				if digit0 = to_unsigned(9,4) then
+					digit0 <= (others => '0');
+					digit1 <= digit1 + 1;
+					
+					if digit1 = to_unsigned(9,4) then
+						digit1 <= (others => '0');
+						digit2 <= digit2 + 1;
+						
+						if digit2 = to_unsigned(9,4) then
+							digit2 <= (others => '0');
+							digit3 <= digit3 + 1;
+							
+							if digit3 = to_unsigned(9,4) then
+								digit3 <= (others => '0');
+							end if;
+						end if;
+					end if;
+				end if;
+			end if;
 		end if;
 	end process;
 	
-	
 	Inst_sevenseg: sevenseg PORT MAP(
 		clk => clk,
-		digit0 => counter(21 downto 18),
-		digit1 => counter(25 downto 22),
-		digit2 => counter(29 downto 26),
-		digit3 => counter(33 downto 30),
+		digit0 => digit0,
+		digit1 => digit1,
+		digit2 => digit2,
+		digit3 => digit3,
 		digit_sel => digits,
 		segments => segments
 	);
